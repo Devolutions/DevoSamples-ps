396335c8ed0b48a55ca4bb0873d6baeabccfcfe4
Érica Poirier <62104574+erica-poirier@users.noreply.github.com>
2025-09-23 15:19:29 -0400

    Structure_reword_v4

--- RDM/entries/MassEdits/Edit-EntryPassword-All-Vaults.ps1
+++ RDM/entries/MassEdits/Edit-EntryPassword-All-Vaults.ps1
@@ -32,15 +32,12 @@ $ErrorActionPreference = 'SilentlyContinue'
 
 #check if RDM PS module is installed
 if(-not (Get-Module Devolutions.PowerShell -ListAvailable)){
-    Install-Module Devolutions.PowerShell -Scope CurrentUser
+  Install-Module Devolutions.PowerShell -Scope CurrentUser
 }
 
 # Adapt the data source name
 $ds = Get-RDMDataSource -Name "NameOfYourDataSourceHere"
 Set-RDMCurrentDataSource $ds
-
-#---------------------------------[Functions]----------------------------------
-
 
 #---------------------------------[Execution]----------------------------------
 
